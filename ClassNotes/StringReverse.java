8253732840077521db6a26189f77330dcb51a413
Risenforce55 <36218272+Risenforce55@users.noreply.github.com>
2019-03-20 15:11:26 -0500

    htjmuj.,uiyf

---
+++ ClassNotes/StringReverse.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class StringReverse here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StringReverse
+{
+    public static String reverse(String str)
+    {
+        // must have a terminating case
+        if(str.equals(""))
+        {
+            return str;
+        }
+        
+        // must make the problem simpler
+        String firstChar = str.substring(0,1);
+        String restOfString = str.substring(1);
+        
+        //must recurse
+        //      call this method recursively with a simpler problem
+        String restOfStringReversed = reverse(restOfString);
+        
+        //return the solution
+        String strReversed = restOf
+    }
+}
